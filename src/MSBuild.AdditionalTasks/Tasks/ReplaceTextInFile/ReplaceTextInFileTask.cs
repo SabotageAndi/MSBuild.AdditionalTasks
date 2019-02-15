1b5b234eb29a518af783c40d2db39b6bdc3f91fc
Andreas Willich <andreas.willich@gmail.com>
2019-02-15 21:49:31 +0100

    first infrastructure

---
+++ src/MSBuild.AdditionalTasks/Tasks/ReplaceTextInFile/ReplaceTextInFileTask.cs
@@ -0,0 +1,13 @@
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.AdditionalTasks
+{
+    public class ReplaceTextInFileTask : Task
+    {
+        public override bool Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
