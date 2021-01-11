da0d0085659ceaf9fe9d0b11e80aa245592ce9d1
Balazs Epresi <b.epresi@tricentis.com>
2021-01-11 15:26:49 +0100

    Improve ReplaceTextInFileTask with locking and WriteOnlyWhenChanged

--- src/MSBuild.AdditionalTasks/Tasks/ReplaceTextInFile/ReplaceTextInFileTask.cs
+++ src/MSBuild.AdditionalTasks/Tasks/ReplaceTextInFile/ReplaceTextInFileTask.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -12,12 +13,32 @@ namespace MSBuild.AdditionalTasks
         [Required] public string OutputFile { get; set; }
         [Required] public string TextToReplace { get; set; }
         [Required] public string TextToReplaceWith { get; set; }
+        public bool WriteOnlyWhenChanged { get; set; }
+
+        private static ReaderWriterLockSlim _readWriteLock = new ReaderWriterLockSlim();
+
         public override bool Execute()
         {
+            _readWriteLock.EnterWriteLock();
             try
             {
                 var fileContent = File.ReadAllText(InputFile);
                 var replacedContent = fileContent.Replace(TextToReplace, TextToReplaceWith);
+                if (WriteOnlyWhenChanged && File.Exists(OutputFile))
+                {
+                    var existingContent = File.ReadAllText(OutputFile);
+                    if (replacedContent.Equals(existingContent))
+                    {
+                        Log.LogMessage("Skipping unchanged file {0}", OutputFile);
+                        return true;
+                    }
+                }
+
+                var dir = Path.GetDirectoryName(OutputFile);
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
 
                 File.WriteAllText(OutputFile, replacedContent);
 
@@ -27,6 +48,10 @@ namespace MSBuild.AdditionalTasks
             {
                 Log.LogErrorFromException(e);
                 return false;
+            }
+            finally
+            {
+                _readWriteLock.ExitWriteLock();
             }
         }
     }
